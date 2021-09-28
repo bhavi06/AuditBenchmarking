0372f9dd95fcc6ab04ea012a2611206677f12321
bhavi06 <58685725+bhavi06@users.noreply.github.com>
2021-09-28 09:25:55 +0530

    First Commit

---
+++ Repository/BenchmarkRepo.cs
@@ -0,0 +1,43 @@
+﻿using AuditBenchmarkModule.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuditBenchmarkModule.Repository
+{
+    public class BenchmarkRepo : IBenchmarkRepo
+    {
+        private readonly log4net.ILog log4netval = log4net.LogManager.GetLogger(typeof(BenchmarkRepo));
+        private static List<AuditBenchmark> auditBenchmarkList = new List<AuditBenchmark>()
+        {
+            new AuditBenchmark
+            {
+                AuditType="Internal",
+                BenchmarkNoAnswers=3
+            },
+            new AuditBenchmark
+            {
+                AuditType="SOX",
+                BenchmarkNoAnswers=1
+            }
+        };
+        public List<AuditBenchmark> GetNolist()
+        {
+            log4netval.Info(" Http GET request " + nameof(BenchmarkRepo));
+
+            List<AuditBenchmark> listOfCriteria = new List<AuditBenchmark>();
+            try
+            {
+                listOfCriteria = auditBenchmarkList;
+                return listOfCriteria;
+            }
+            catch (Exception e)
+            {
+                log4netval.Error(" Exception here" + e.Message + " " + nameof(BenchmarkRepo));
+                return null;
+            }
+
+        }
+    }
+}
---
+++ Services/BenchmarkService.cs
@@ -0,0 +1,39 @@
+﻿using AuditBenchmarkModule.Models;
+using AuditBenchmarkModule.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuditBenchmarkModule.Providers
+{
+    public class BenchmarkService : IBenchmarkService
+    {
+        private readonly log4net.ILog log4netval;
+        private IBenchmarkRepo objBenchmarkRepo;
+        public BenchmarkService(IBenchmarkRepo objBenchmarkRepo)
+        {
+            log4netval = log4net.LogManager.GetLogger(typeof(BenchmarkService));
+            this.objBenchmarkRepo = objBenchmarkRepo;
+        }
+
+        public List<AuditBenchmark> GetBenchmark()
+        {
+            log4netval.Info(" Http GET request " + nameof(BenchmarkService));
+
+            List<AuditBenchmark> listOfRepository = new List<AuditBenchmark>();
+            try
+            {
+                listOfRepository = objBenchmarkRepo.GetNolist();
+                return listOfRepository;
+            }
+            catch (Exception e)
+            {
+                log4netval.Error(" Exception here" + e.Message + " " + nameof(BenchmarkService));
+                return null;
+            }
+
+        }
+
+    }
+}
